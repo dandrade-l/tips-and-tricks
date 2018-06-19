26da62d8698f6921477599bd2e608f596e97194e
Neil Manvar <neil@sentry.io>
2018-06-18 22:47:25 -0700

    Fingerprint via dataCallback

---
+++ js/fingerprint.js
@@ -0,0 +1,22 @@
+var Raven = require('raven');
+
+Raven
+    .config('https://<key>@sentry.io/<project>', {
+        dataCallback: function (data) {
+            // perform conditional on data object
+            if (data.message === "Some message/error...") {
+                // override fingerprint
+                data.fingerprint = ['Some-message-error', 'etc'];
+
+                // or, add specivity (append fingerprint)
+                // data.fingerprint = ['{{ default }}', 'Some-message-error'];
+            }
+            return data;
+        }
+    })
+    .install();
+
+Raven.captureMessage('Some message/error...');
+
+var x = {};
+x.something(); // this error should be shot up to Sentry
